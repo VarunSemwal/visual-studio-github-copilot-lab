2bd499ffd807cb4a66950d0b34b881b4e05d3af4
Varun Semwal <varunsemwal07@gmail.com>
2026-02-15 19:10:31 +0530

    Added post, put and delete endpoints in Product Endpoint

--- src/Products/Endpoints/ProductEndpoints.cs
+++ src/Products/Endpoints/ProductEndpoints.cs
@@ -17,6 +17,64 @@ public static class ProductEndpoints
         .WithName("GetAllProducts")
         .Produces<List<Product>>(StatusCodes.Status200OK);
 
+        //get existing product by id
+        group.MapGet("/{id}", async (int id, ProductDataContext db) =>
+        {
+            return await db.Product.FindAsync(id)
+                is Product model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetProductById")
+        .Produces<Product>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
 
+        // create a new product
+        group.MapPost("/", async (Product product, ProductDataContext db) =>
+        {
+            db.Product.Add(product);
+            await db.SaveChangesAsync();
+
+            return Results.CreatedAtRoute("GetProductById", new { id = product.Id }, product);
+        })
+        .WithName("CreateProduct")
+        .Produces<Product>(StatusCodes.Status201Created);
+
+        // update an existing product
+        group.MapPut("/{id}", async (int id, Product updatedProduct, ProductDataContext db) =>
+        {
+            var existing = await db.Product.FindAsync(id);
+            if (existing is null)
+                return Results.NotFound();
+
+            // update fields
+            existing.Name = updatedProduct.Name;
+            existing.Description = updatedProduct.Description;
+            existing.Price = updatedProduct.Price;
+            existing.ImageUrl = updatedProduct.ImageUrl;
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("UpdateProduct")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound);
+
+        // delete a product
+        group.MapDelete("/{id}", async (int id, ProductDataContext db) =>
+        {
+            var existing = await db.Product.FindAsync(id);
+            if (existing is null)
+                return Results.NotFound();
+
+            db.Product.Remove(existing);
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("DeleteProduct")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound);
     }
 }
